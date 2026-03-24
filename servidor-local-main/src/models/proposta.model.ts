0f4d262b464f55cafe2361f1b916b893ff1c2826
Fábio Barros <brainsonfire85@gmail.com>
2026-03-24 10:07:49 -0100

    change: proposta e orcamento

--- servidor-local-main/src/models/proposta.model.ts
+++ servidor-local-main/src/models/proposta.model.ts
@@ -1,1 +1,105 @@
 
+import db from "../lib/db.js";
+import type { propostaDBType } from "../utils/types.js";
+
+export const propostaModel = {
+    async create(newProposta: propostaDBType) {
+        try {
+            const query = `INSERT INTO tabela_proposta VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
+
+            const values = [
+                null,
+                newProposta.id_prestacao,
+                newProposta.preco_hora,
+                newProposta.horas_estimadas,
+                newProposta.estado,
+                newProposta.enabled,
+                new Date(),
+                new Date(),
+            ];
+
+            const rows = await db.execute(query, values);
+
+            return rows;
+        } catch (error) {
+            console.log(error);
+            return null;
+        }
+    },
+
+    async getAll() {
+        try {
+            const query = `SELECT * FROM tabela_proposta`;
+
+            const [rows] = await db.execute(query);
+
+            return Array.isArray(rows) && rows.length > 0 ? rows[0] : [];
+        } catch (error) {
+            console.log(error);
+            return null;
+        }
+    },
+
+    async get(id: string) {
+        try {
+            const query = `SELECT * FROM tabela_proposta WHERE id = ?`;
+
+            const value = [id];
+
+            const rows = await db.execute(query, value);
+
+            return Array.isArray(rows) && rows.length > 0 ? rows[0] : null;
+        } catch (error) {
+            console.log(error);
+            return null;
+        }
+    },
+
+    async update(id: string, updatedProposta: propostaDBType) {
+        try {
+            const query = `UPDATE tabela_proposta
+                        SET
+                            id_prestacao_servico=?,
+                            preco_hora=?,
+                            horas_estimadas=?,
+                            estado=?,
+                            enabled=?,
+                            updated_at=?
+                        WHERE
+                            id=?
+                        ;`;
+
+            const values = [
+                updatedProposta.id_prestacao,
+                updatedProposta.preco_hora,
+                updatedProposta.horas_estimadas,
+                updatedProposta.estado,
+                updatedProposta.enabled,
+                new Date(),
+                id,
+            ];
+
+            const rows = await db.execute(query, values);
+
+            return rows;
+        } catch (error) {
+            console.log(error);
+            return null;
+        }
+    },
+
+    async delete(id: string) {
+        try {
+            const query = `DELETE FROM tabela_proposta WHERE id = ?`;
+
+            const value = [id];
+
+            const rows: any = await db.execute(query, value);
+
+            return rows[0]?.affectedRows === 0 ? null : rows;
+        } catch (error) {
+            console.log(error);
+            return null;
+        }
+    }
+};
