4eced2c827277afaa4f954843c2f92467cd0c4e4
Fábio Barros <brainsonfire85@gmail.com>
2026-03-31 12:36:32 -0100

    feat: implement user authentication, JWT middleware, and Swagger API documentation

--- servidor-local-main/src/routes/user.route.ts
+++ servidor-local-main/src/routes/user.route.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { UserController } from "../controllers/user.controller.js";
+import { AuthMiddleware } from "../security/auth.middleware.js";
 
 const UsersRouter = {
     create: "/create",
@@ -7,14 +8,16 @@ const UsersRouter = {
     getAll: "/",
     update: "/update/:id",
     delete: "/delete/:id",
+    login: "/login"
 }
 
 const router = Router()
 
-router.get(UsersRouter.getAll, UserController.getAll)
+router.get(UsersRouter.getAll, AuthMiddleware, UserController.getAll)
 router.get(UsersRouter.getById, UserController.get)
 router.post(UsersRouter.create, UserController.create)
 router.put(UsersRouter.update, UserController.update)
 router.delete(UsersRouter.delete, UserController.delete) 
+router.post(UsersRouter.login, UserController.login)
 
 export { router };
